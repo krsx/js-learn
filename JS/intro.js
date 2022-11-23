70dcc4b055a06a552ce1cc34d080fae97199de7b
Krisna <krisnaerlangga08@gmail.com>
2022-11-24 00:25:17 +0700

    another start :tada:

---
+++ JS/intro.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+document.writeln("Hello World");
+
+document.writeln("<br>");
+document.writeln("Nama: " + "Krisna Erlangga");
+
+alert("Hello World");
+
+const max = 57;
+let actual;
+actual = max - 13;
+let percentage;
+percentage = actual / max;
+console.log(percentage);
+let test = "Krisna Erlangga adalah orang dengan nomor " + max;
+console.log(test);
+
+let result;
+
+let a = 5,
+  b = 10;
+result = a + b;
+alert(result);
