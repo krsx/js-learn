b961c7596479952366e6eb07b37d2b220bd89154
Krisna <krisnaerlangga08@gmail.com>
2022-12-05 16:58:24 +0700

    finish add todo :rocket:

--- projects/simple-todo/app.js
+++ projects/simple-todo/app.js
@@ -5,31 +5,49 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
 
 //Functions
 function addTodo(event) {
   event.preventDefault();
   //Div for todo
   const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+  todoDiv.classList.add(
+    "todo",
+    "bg-white",
+    "border",
+    "flex",
+    "py-6",
+    "px-5",
+    "rounded-lg",
+    "shadow-sm",
+    "mb-2"
+  );
 
   //Create Li
   const newTodo = document.createElement("li");
-  newTodo.innerText = "hey";
-  newTodo.classList.add("todo-item");
+  newTodo.innerText = todoInput.value; //value sesuai dengan input form
+  newTodo.classList.add("todo-item", "flex-1");
   todoDiv.appendChild(newTodo);
 
   //Complete button
   const completeButton = document.createElement("button");
   completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-  completeButton.classList.add("complete-btn");
+  completeButton.classList.add("complete-btn", "text-green-500", "mr-6");
   todoDiv.appendChild(completeButton);
 
   //Delete button
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-  deleteButton.classList.add("delete-btn");
+  deleteButton.classList.add("delete-btn", "text-red-500");
   todoDiv.appendChild(deleteButton);
 
+  //Create todolist itself
   todoList.append(todoDiv);
+
+  todoInput.value = "";
+}
+
+function deleteTodo(event) {
+  const item = event.target;
 }
